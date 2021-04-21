6766bddab06a13cc503c9397b74736cb29a08edf
chan y <1904845564@qq.com>
2021-04-21 22:57:16 +0800

    创建环境

---
+++ xkl-app/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+import { Button } from 'vant';
+Vue.use(Button);
+
+import 'lib-flexible/flexible'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
